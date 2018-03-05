a89e04686e0fa68f8deb9a2768d29f980e0618c2
Alexanderklau <429095816@qq.com>
2018-03-05 21:04:58 +0800

    fix

--- django-blog/blog_project/blog/views.py
+++ django-blog/blog_project/blog/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+import logging
+from django.conf import settings
 
+logger = logging.getLogger('blog.views')
 # Create your views here.
 
+
+def global_setting(request):
+    return {"SITE_NAME":settings.SITE_NAME,
+            "SITE_DESC":settings.SITE_DESC}
+
 def index(request):
+    try:
+        file = open('ss.js', "r")
+    except Exception as e:
+        logger.error(e)
     return render(request, 'index.html', locals())
--- django-blog/blog_project/blog_project/settings.py
+++ django-blog/blog_project/blog_project/settings.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 """
 Django settings for blog_project project.
 
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'blog.views.global_setting'
             ],
         },
     },
@@ -104,3 +106,91 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+# 网站的基本信息配置
+SITE_NAME = "xxx个人博客"
+SITE_DESC = "python开发!!!!"
+WEIBO = "http://123.com"
+QQ = "123456789"
+RSS = "pro!!!!!"
+EMAILS = "123@qq.com"
+
+
+
+# 自定义日志输出信息
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(threadName)s:%(thread)d] [%(name)s:%(lineno)d] [%(module)s:%(funcName)s] [%(levelname)s]- %(message)s'}  #日志格式
+    },
+    'filters': {
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
+            },
+        'default': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': 'log/all.log',                #日志输出文件
+            'maxBytes': 1024*1024*5,                  #文件大小
+            'backupCount': 5,                         #备份份数
+            'formatter':'standard',                   #使用哪种formatters日志格式
+        },
+        'error': {
+            'level':'ERROR',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': 'log/error.log',
+            'maxBytes':1024*1024*5,
+            'backupCount': 5,
+            'formatter':'standard',
+            },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+        'request_handler': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': 'log/script.log',
+            'maxBytes': 1024*1024*5,
+            'backupCount': 5,
+            'formatter':'standard',
+            },
+        'scprits_handler': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename':'log/script.log',
+            'maxBytes': 1024*1024*5,
+            'backupCount': 5,
+            'formatter':'standard',
+            }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['default', 'console'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'django.request': {
+            'handlers': ['request_handler'],
+            'level': 'DEBUG',
+            'propagate': False,
+            },
+        'scripts': {
+            'handlers': ['scprits_handler'],
+            'level': 'INFO',
+            'propagate': False
+        },
+        'blog.views': {
+            'handlers': ['default', 'error'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+    }
+}
