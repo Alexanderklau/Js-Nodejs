fcb335ca7cdcf2c3c6eef80d54ef43b6589d59bb
Alexanderklau <429095816@qq.com>
2018-03-08 15:16:32 +0800

    fix

--- TWebchat/chatdemo.py
+++ TWebchat/chatdemo.py
@@ -5,7 +5,11 @@ __author__ = 'lau.wenbo'
 
 import tornado.ioloop
 import tornado.options
+import tornado.websocket
 import tornado.web
+import tornado.escape
+import uuid
+import logging
 import os.path
 from tornado.options import define, options
 
@@ -17,6 +21,7 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", MainHandler),
+            (r"/chatsocket", ChatSocketHandler)
         ]
         settings = dict(
             cookie_secret = "You_CANT_GUESS_MY_SECRET",
@@ -30,6 +35,40 @@ class Application(tornado.web.Application):
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
+
+
+class ChatSocketHandler(tornado.websocket.WebSocketHandler):
+    waiters = set()
+
+    def open(self):
+        ChatSocketHandler.waiters.add(self)
+
+    @classmethod
+    def send_update(cls, chat):
+        logging.info("sending message to %d waiters", len(cls.waiters))
+        for waiter in cls.waiters:
+            try:
+                waiter.write_message(chat)
+            except:
+                logging.error("Error sending message", exc_info=True)
+
+
+    def on_close(self):
+        ChatSocketHandler.waiters.remove(self)
+
+
+    def on_message(self, message):
+        logging.info("got message %r", message)
+        print(message)
+        parsed = tornado.escape.json_decode(message)
+        self.username = parsed["username"]
+        chat = {
+            "id": str(uuid.uuid4()),
+            "body": parsed["body"],
+            "type": "message",
+        }
+        chat["html"] = tornado.escape.to_basestring(self.render_string("message.html", message=chat))
+        ChatSocketHandler.send_update(chat)
 
 
 def main():
